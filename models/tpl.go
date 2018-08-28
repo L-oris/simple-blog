1a396c42524af7fd5a7ad2b9f2d0f81bdd06014d
Loris <loris4526@gmail.com>
2018-08-28 09:24:46 +0200

    parse templates from multiple directories

--- models/tpl.go
+++ models/tpl.go
@@ -2,11 +2,34 @@ package models
 
 import (
 	"html/template"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 // TPL holds a reference to all templates
 var TPL *template.Template
 
 func init() {
-	TPL = template.Must(template.ParseGlob("templates/*.gohtml"))
+	TPL = template.Must(parseTemplates())
+}
+
+func parseTemplates() (*template.Template, error) {
+	templ := template.New("")
+	err := filepath.Walk("./templates", func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(path, ".gohtml") {
+			_, err = templ.ParseFiles(path)
+			if err != nil {
+				return err
+			}
+		}
+		return err
+	})
+
+	if err != nil {
+		// TODO: better error message, wrap it
+		return nil, err
+	}
+
+	return templ, nil
 }
