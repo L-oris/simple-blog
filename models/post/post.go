ea00b23de5d6b09e8059089e05075d5c489eda1d
Loris <loris4526@gmail.com>
2018-09-04 08:36:25 +0200

    unit tests for 'post.go'

--- models/post/post.go
+++ models/post/post.go
@@ -16,12 +16,25 @@ type Post struct {
 	CreatedAt time.Time `json:"createdAt"`
 }
 
+// IsValid checks whether Post has all the required fields filled with meaningful values
+func IsValid(p Post) bool {
+	if p.ID == "" || p.Title == "" || p.Content == "" || p.CreatedAt.IsZero() {
+		return false
+	}
+	return true
+}
+
 // FromJSON creates a Post out of JSON
 func FromJSON(jsonPost []byte) (Post, error) {
 	var newPost Post
 	err := json.Unmarshal(jsonPost, &newPost)
 	if err != nil {
 		errMessage := "models.PostFromJSON > unmarshal error: " + err.Error()
+		return Post{}, errors.New(errMessage)
+	}
+
+	if !newPost.HasTitleAndContent() {
+		errMessage := "models.PostFromJSON > Post provided is missing 'Title' or 'Content'"
 		return Post{}, errors.New(errMessage)
 	}
 
--- models/post/post_test.go
+++ models/post/post_test.go
@@ -1,27 +1,100 @@
 package post
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+	"time"
+)
+
+var postOnlyTitle = Post{
+	Title: "some title",
+}
+var postOnlyContent = Post{
+	Content: "some content",
+}
+var postWithTitleAndContent = Post{
+	Title:   "some title",
+	Content: "some content",
+}
+var postComplete = Post{
+	ID:        "some id",
+	Title:     "some title",
+	Content:   "some content",
+	CreatedAt: time.Date(2009, 11, 17, 20, 34, 58, 651387237, time.UTC),
+}
 
 func TestHasTitleAndContent(t *testing.T) {
-	postOnlyTitle := Post{
-		Title: "some title",
-	}
 	if postOnlyTitle.HasTitleAndContent() == true {
 		t.Error("Expected missing 'Title' to be found")
 	}
 
-	postOnlyContent := Post{
-		Content: "some content",
-	}
 	if postOnlyContent.HasTitleAndContent() == true {
 		t.Error("Expected missing 'Content' to be found")
 	}
 
-	postWithTitleAndContent := Post{
-		Title:   "some title",
-		Content: "some content",
-	}
 	if postWithTitleAndContent.HasTitleAndContent() == false {
 		t.Error("Expected Post with 'Title' and 'Content' to be valid")
+	}
+}
+
+func TestGenerateFromPartial(t *testing.T) {
+	_, err := GenerateFromPartial(postOnlyTitle)
+	if err == nil {
+		t.Error("Expected to throw if Post is missing required fields")
+	}
+
+	result, _ := GenerateFromPartial(postWithTitleAndContent)
+	if result.ID == "" {
+		t.Error("Expected ID to be generated for new Post")
+	}
+
+	result, _ = GenerateFromPartial(postComplete)
+	if result.ID == postComplete.ID {
+		t.Error("Expected new ID to replace old one")
+	}
+
+	result, _ = GenerateFromPartial(postWithTitleAndContent)
+	if result.CreatedAt == postWithTitleAndContent.CreatedAt {
+		t.Error("Expected Date to be generated for new Post")
+	}
+
+	result, _ = GenerateFromPartial(postComplete)
+	if result.CreatedAt == postComplete.CreatedAt {
+		t.Error("Expected current Date to replace old one")
+	}
+}
+
+func TestFromJSON(t *testing.T) {
+	badJSON := []byte{1, 2, 3}
+	_, err := FromJSON(badJSON)
+	if err == nil {
+		t.Error("Expected bad JSON to be catched")
+	}
+
+	badPost := struct {
+		Field string
+	}{
+		Field: "some",
+	}
+	badPostJSON, _ := json.Marshal(badPost)
+	_, err = FromJSON(badPostJSON)
+	if err == nil {
+		t.Error("Expected bad JSON to be catched")
+	}
+
+	postCompleteJSON, _ := json.Marshal(postComplete)
+	_, err = FromJSON(postCompleteJSON)
+	if err != nil {
+		t.Error("Expected good JSON to be valid")
+	}
+}
+
+func TestIsValid(t *testing.T) {
+	if IsValid(postOnlyTitle) == true {
+		t.Error("Expected Post with empty fields to be catched")
+	}
+
+	if IsValid(postComplete) == false {
+		t.Error("Expected Post to be valid")
 	}
 }
